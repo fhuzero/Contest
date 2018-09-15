ed6427440ae89070d04ca4b1e9cbfcf349fc611b
Fang Hu <37891485+fhuzero@users.noreply.github.com>
2018-09-15 10:48:48 +0800

    determine whether the list is empty

--- Contest/scheduling.h
+++ Contest/scheduling.h
@@ -11,33 +11,39 @@ CNC* getDest(list<CNC*> waitLoadList, list<CNC*> processList, list<CNC*> waitUnl
 	int max_waitCNC = (*waitLoadList.begin())->workRemainTime,
 		max_Pos = (*waitLoadList.begin())->Pos;  //actually the minimum value
 
-
+	
 	CNC *ret_CNC = *waitLoadList.begin();
-	for (auto iter = waitLoadList.begin(); iter != waitLoadList.end(); iter++)
-	{
-		if ((*iter)->workRemainTime < max_waitCNC)
+	if(!waitLoadList.empty()){
+		for (auto iter = waitLoadList.begin(); iter != waitLoadList.end(); iter++)
 		{
-			max_waitCNC = (*iter)->workRemainTime;
-			//max_Pos = *iter->Pos;
-			ret_CNC = *iter;
+			if ((*iter)->workRemainTime < max_waitCNC)
+			{
+				max_waitCNC = (*iter)->workRemainTime;
+				//max_Pos = *iter->Pos;
+				ret_CNC = *iter;
+			}
 		}
 	}
-	for (auto iter = processList.begin(); iter != processList.end(); iter++)
-	{
-		if ((*iter)->workRemainTime < max_waitCNC)
+	if(!processList.empty()){
+		for (auto iter = processList.begin(); iter != processList.end(); iter++)
 		{
-			max_waitCNC = (*iter)->workRemainTime;
-			//max_Pos = *iter->Pos;
-			ret_CNC = *iter;
+			if ((*iter)->workRemainTime < max_waitCNC)
+			{
+				max_waitCNC = (*iter)->workRemainTime;
+				//max_Pos = *iter->Pos;
+				ret_CNC = *iter;
+			}
 		}
 	}
-	for (auto iter = waitUnloadList.begin(); iter != waitUnloadList.end(); iter++)
-	{
-		if ((*iter)->workRemainTime < max_waitCNC)
+	if(!waitUnloadList.empty()){
+		for (auto iter = waitUnloadList.begin(); iter != waitUnloadList.end(); iter++)
 		{
-			max_waitCNC = (*iter)->workRemainTime;
-			//max_Pos = *iter->Pos;
-			ret_CNC = *iter;
+			if ((*iter)->workRemainTime < max_waitCNC)
+			{
+				max_waitCNC = (*iter)->workRemainTime;
+				//max_Pos = *iter->Pos;
+				ret_CNC = *iter;
+			}
 		}
 	}
 	return ret_CNC;
